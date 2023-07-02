f73a859cf35503b734e90215cfe71f478d97db7e
Alex Ulanch <alexulanch@gmail.com>
2023-07-01 21:38:31 -0400

    Fix typo

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/alexanderulanch/terraform-modules//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name           = "webserver-stage"
   db_remote_state_bucket = "terraform-state-876253587652314"
@@ -11,7 +11,7 @@ module "webserver_cluster" {
 
   instance_type = "t2.micro"
   min_size      = 2
-  max-size      = 10
+  max_size      = 10
 }
 
 
